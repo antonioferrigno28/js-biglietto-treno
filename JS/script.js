2ae5f9b850cb17ba37c895c24575f3b6c20deb1d
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-14 13:36:04 +0200

    Pseudocodice italiano

---
+++ JS/script.js
@@ -0,0 +1,31 @@
+/*NOSTRI DATI
+     costo del biglietto è definito in base ai km (0.21 € al km)
+     va applicato uno sconto del 20% per i minorenni
+     va applicato uno sconto del 40% per gli over 65
+     output in forma umana (due decimali dopo la virgola, come i normali prezzi di qualsiasi cosa)
+    */
+
+/*RACCOLTA DATI
+     chiedere all'utente il numero di chilometri che vuole percorrere e lo trasformo in numero
+     chiedere all'utente l'età del passeggero
+    */
+
+/*ELABORAZIONE
+    controllo che il numero dei km sia maggiore di 0
+    controllo che l'età sia maggiore di 0
+
+    SE il numero di km è maggiore di 0 AND l'età è maggiore di 0
+        calcolo il prezzo del biglietto
+    ALTRIMENTI 
+        stampo un messaggio di errore
+
+    SE  l'età è minore di 18
+        applico al prezzo del biglietto uno sconto del 20%
+
+    SE l'età è maggiore di 65
+        applico al prezzo del biglietto uno sconto del 40%
+    */
+
+/*OUTPUT
+    stampo il prezzo FINALE del biglietto
+    */
