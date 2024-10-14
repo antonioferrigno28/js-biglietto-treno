fb5d1c3f1b5c8686bfb2edbf528f56448e85330c
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-14 15:25:28 +0200

    DONE! (added NaN control, fixed double error message)

--- JS/script.js
+++ JS/script.js
@@ -18,14 +18,23 @@ let userAge = parseInt(prompt("Inserisci l'eta del passeggero"));
 //dichiaro una variabile per il prezzo del biglietto
 let ticketPrice = userKm * 0.21;
 
+//Controllo se il valore sia un numero valido
+if (isNaN(userKm) || isNaN(userAge)) {
+  // stampo un messaggio di errore
+  alert(
+    "Inserisci un numero positivo o maggiore di zero sia per i km che per l'eta"
+  );
+}
+
 //SE il numero di km è maggiore di 0 AND l'età è compresa tra 18 e 65
 if (userAge >= 18 && userAge <= 65) {
   // stampo il prezzo del biglietto
   console.log("Il prezzo del biglietto é " + ticketPrice.toFixed(2) + "€");
+  // ALTRIMENTI SE il numero di km è minore di 0 o l'età è minore di 0
 } else if (userKm <= 0 || userAge <= 0) {
   // stampo un messaggio di errore
   alert(
-    "Inserisci un numero positivo o maggiore di zero sia per i km che per l'eta"
+    "Inserisci un numero positivo o maggiore di zero sia per i km che per l'eta 1"
   );
 }
 //SE l'età è minore di 18
@@ -43,12 +52,4 @@ else if (userAge > 65) {
   // stampo il prezzo FINALE del biglietto
 
   console.log("Il prezzo finale è " + ticketPriceNew.toFixed(2) + "€");
-}
-
-//ALTRIMENTI
-else {
-  // stampo un messaggio di errore
-  alert(
-    "Inserisci un numero positivo o maggiore di zero sia per i km che per l'età"
-  );
 }
