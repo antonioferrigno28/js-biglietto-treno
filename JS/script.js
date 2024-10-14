1d64feb46b2b51d5d90bb35fc622dd4a2db1ee52
antonioferrigno28 <ferrigno1@hotmail.it>
2024-10-14 13:54:27 +0200

    Primo approccio (ERRORE TICKETPRICE UNDEFINED)

--- JS/script.js
+++ JS/script.js
@@ -5,27 +5,39 @@
      output in forma umana (due decimali dopo la virgola, come i normali prezzi di qualsiasi cosa)
     */
 
-/*RACCOLTA DATI
-     chiedere all'utente il numero di chilometri che vuole percorrere e lo trasformo in numero
-     chiedere all'utente l'età del passeggero
-    */
+//RACCOLTA DATI
+// chiedere all'utente il numero di chilometri che vuole percorrere e lo trasformo in numero
+let userKm = parseInt(
+  prompt("Inserisci il numero di chilometri che vuoi percorrere")
+);
+// chiedere all'utente l'età del passeggero
+let userAge = parseInt(prompt("Inserisci l'eta del passeggero"));
 
-/*ELABORAZIONE
-    controllo che il numero dei km sia maggiore di 0
-    controllo che l'età sia maggiore di 0
+//ELABORAZIONE
 
-    SE il numero di km è maggiore di 0 AND l'età è maggiore di 0
-        calcolo il prezzo del biglietto
-    ALTRIMENTI 
-        stampo un messaggio di errore
+//SE il numero di km è maggiore di 0 AND l'età è maggiore di 0
+if (userKm > 0 && userAge > 0) {
+  // calcolo il prezzo del biglietto
+  let ticketPrice = userKm * 0.21;
+}
+//SE l'età è minore di 18
+else if (userAge < 18) {
+  // applico al prezzo del biglietto uno sconto del 20%
+  ticketPrice = ticketPrice - (ticketPrice * 20) / 100;
+}
 
-    SE  l'età è minore di 18
-        applico al prezzo del biglietto uno sconto del 20%
+//SE l'età è maggiore di 65
+else if (userAge > 65) {
+  // applico al prezzo del biglietto uno sconto del 40%
+  ticketPrice = ticketPrice - (ticketPrice * 40) / 100;
+}
 
-    SE l'età è maggiore di 65
-        applico al prezzo del biglietto uno sconto del 40%
-    */
+//ALTRIMENTI
+else {
+  // stampo un messaggio di errore
+  alert("Inserisci un numero positivo");
+}
 
-/*OUTPUT
-    stampo il prezzo FINALE del biglietto
-    */
+//OUTPUT
+// stampo il prezzo FINALE del biglietto
+console.log("il prezzo del biglietto è: " + ticketPrice.toFixed(2) + "€");
